77c3f45b68ac1bcf1d1492ee733f013b9a4f11d8
juniel2199 <youngs5440@gmail.com>
2024-01-04 14:13:06 +0900

    과제

---
+++ javatest1/src/com/test/question/Q001.java
@@ -0,0 +1,20 @@
+package com.test.question;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q001 {
+public static void main(String[] args) throws IOException {
+	
+	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	
+	System.out.println("태어난 년도 : ");
+	int year; 
+	int age;
+	
+	
+	
+	
+}
+}
---
+++ javatest1/src/com/test/question/Q006.java
@@ -0,0 +1,7 @@
+package com.test.question;
+
+public class Q006 {
+public static void main(String[] args) {
+	
+}
+}
