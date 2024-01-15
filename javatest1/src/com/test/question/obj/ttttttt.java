4e2ffc350154115bedcf734215f6d70c1b7ecee4
juniel99 <youngs5440@gmail.com>
2024-01-16 08:46:47 +0900

    test

--- javatest1/src/com/test/question/obj/ttttttt.java
+++ javatest1/src/com/test/question/obj/ttttttt.java
@@ -4,25 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ttttttt {
-
+public class ttttttt{
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
-	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	
-	
-	String txt = "";
-	System.out.print("값을 입력하시오: ");
-	
-	txt = reader.readLine();
-	System.out.printf("%s하세요",txt);
+		System.out.println(0b0111);
+		
+	}
 
-	
-	
-	
-	
-	}//mAIN
-	
-	
-}
+ }
