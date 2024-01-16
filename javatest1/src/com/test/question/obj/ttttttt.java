713b2f490e4d52f8860dc8ac0e9e7deae6428277
juniel99 <youngs5440@gmail.com>
2024-01-17 08:54:55 +0900

    .

--- javatest1/src/com/test/question/obj/ttttttt.java
+++ javatest1/src/com/test/question/obj/ttttttt.java
@@ -7,9 +7,15 @@ import java.io.InputStreamReader;
 public class ttttttt{
 	
 	public static void main(String[] args) {
-		
-		System.out.println(0b0111);
-		
+		int a = 10;
+
+		int b = 5;
+
+		System.out.println(a & b); // 10 > 1010 (2^3 + 2^1) , 5 > 0101 (2^2 + 2^0) 엔드이므로 1010 , 0101 비교 > 0000
+
+		System.out.println(a | b); // 위에 숫자 계산 1111 > 8 + 4 + 2 + 1 = 15
+	
+	
 	}
 
  }
