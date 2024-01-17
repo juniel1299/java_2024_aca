65a8cb2c0eb84845167a411dd0ffb760b4f33760
juniel99 <youngs5440@gmail.com>
2024-01-17 17:10:36 +0900

    0117

---
+++ javatest1/src/com/test/java/collection/MyHashMap.java
@@ -0,0 +1,27 @@
+package com.test.java.collection;
+
+public class MyHashMap {
+
+	//key + value 
+	//case 1. 배열 2개 
+	
+//	private String[] keys;
+//	private String[] values;
+	
+//	private int index;
+
+	//case.2
+	private Entry[] list;
+	private int index;
+	
+}
+
+class Entry{
+	private String key;
+	private String value;
+}
+
+
+
+
+
