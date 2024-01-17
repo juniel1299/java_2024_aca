f44a0d95dc2463151894b50819a95cd01ac4a22b
juniel99 <youngs5440@gmail.com>
2024-01-17 12:57:31 +0900

    0117

--- javatest1/src/com/test/java/collection/Ex54_ArrayList.java
+++ javatest1/src/com/test/java/collection/Ex54_ArrayList.java
@@ -4,55 +4,232 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Ex54_ArrayList {
+	
 	public static void main(String[] args) {
-
-		/*
-		 
-		 배열 + 컬렉션 + 제어문 + 클래스
-		 
-		 컬렉션 , Collection
-		 JCF , java Collection Framework
-		 (향상된)배열
-		 향상?
-		 	1.성능 향상
-		 	2.사용법 향상
-		 	3.길이 가변 > 배열의 길이가 늘었다 줄었다 가능 
-		 	
-		 	
-		 컬렉션 종류
-		 1. List 계열
-		 	- ArrayList(*****)
-		 	- LinkedList
-		 	- Queue
-		 	- Stack
-		 	- Vector(-)
-		 	
-		 2. Set 계열
-		 	- HashSet(***)
-		 	- TreeSet
-		 	
-		 3. Map 계열
-		 	- HashMap(*****)
-		 	- TreeMap
-		 	- Properties(-)
-		 	- HashTable(-)
 		
-		 	
-		 	ArrayList 클래스
-		 	- Iterable<E>, Collection<E>, List<E>, 
-		 	- Resizable-array implementation of the List interface
-		 	- 순수 배열하고 구조가 가장 유사함
-		 	
-		 */
+		//com.test.java.collection > Ex54_ArrayList.java
+		
+		/*
+		
+			배열 + 컬렉션 + 제어문 + 클래스
+			
+		
+			컬렉션, Collection
+			- JCF, Java Collection Framework
+			- (향상된) 배열
+			- 향상?
+				1. 성능 향상
+				2. 사용법 향상
+				3. 길이 가변 > 배열의 길이가 늘었다 줄었다 가능
+				
+			
+			컬렉션 종류
+			1. List 계열
+				- ArrayList(*****)
+				- LinkedList
+				- Queue
+				- Stack
+				- Vector(-)
+			
+			2. Set 계열
+				- HashSet(***)
+				- TreeSet
+			
+			3. Map 계열
+				- HashMap(*****)
+				- TreeMap
+				- Properties(-)
+				- HashTable(-)
+				
+			
+			ArrayList 클래스
+			- Iterable<E>, Collection<E>, List<E>
+			- Resizable-array implementation of the List interface
+			- 순수 배열하고 구조가 가장 유사함.
+		
+		*/
 		
 		//m1();
 		//m2();
 		//m3();
 		//m4();
 		//m5();
-		m6();
+		//m6();
+		//m7();
+		m8();
 		
-	}// main
+		
+	}
+
+	private static void m8() {
+		
+		//배열 생성
+		MyArrayList list = new MyArrayList();
+		
+		System.out.println(list);
+
+		//추가
+		list.add("홍길동");
+		list.add("아무개");
+		list.add("하하하");
+		//list.add("호호호");
+		System.out.println(list);
+		
+//		list.add("후후후");
+//		list.add("헤헤헤");
+//		list.add("가가가");
+//		list.add("나나나");
+//		System.out.println(list);
+//		
+//		list.add("다다다");
+//		System.out.println(list);
+
+		//읽기
+		System.out.println(list.get(0));
+		System.out.println(list.get(1));
+		System.out.println(list.get(2));
+//		System.out.println(list.get(3));
+
+		//개수
+		System.out.println(list.size());
+
+		//탐색 + 읽기
+		for (int i=0; i<list.size(); i++) {
+		      System.out.println(list.get(i));
+		}
+
+		//수정
+		list.set(0, "우하하");
+		System.out.println(list.get(0));
+		
+		System.out.println();
+		System.out.println("삭제 전");
+		System.out.println(list);
+
+		//삭제
+		list.remove(1);
+		
+		System.out.println("삭제 후");
+		System.out.println(list);
+		
+		list.add("가가가");
+		System.out.println(list);
+		
+		list.remove("하하하");
+		System.out.println(list);
+		
+//		for (int i=0; i<list.size(); i++) {
+//		      System.out.println(list.get(i));
+//		}
+
+		//삽입
+		list.add(1, "호호호");
+		list.add(1, "호호호");
+		list.add(1, "호호호");
+		System.out.println(list);
+//		for (int i=0; i<list.size(); i++) {
+//		      System.out.println(list.get(i));
+//		}
+//
+		//검색
+		if (list.indexOf("홍길동") > -1) {
+		      System.out.println("홍길동 있음");
+		} else {
+		      System.out.println("홍길동 없음");
+		}
+		
+		if (list.indexOf("호호호") > -1) {
+		      System.out.println("호호호 있음");
+		} else {
+		      System.out.println("호호호 없음");
+		}
+		System.out.println();
+		
+		
+		System.out.println(list.indexOf("호호호"));
+		System.out.println(list.lastIndexOf("호호호"));
+		
+		System.out.println(list.indexOf("호호호", 2));
+		System.out.println(list.lastIndexOf("호호호", 2));
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		System.out.println(list);
+
+//		//초기화
+		list.clear();
+		System.out.println(list);
+		
+//		System.out.println(list.size());
+		
+		
+		//trimToSize()
+		list.add("홍길동");
+		list.add("아무개");
+		list.add("강아지");
+		list.add("고양이");
+		list.add("병아리");
+		System.out.println(list);
+		
+		list.trimToSize();
+		System.out.println(list);
+		
+		list.add("망아지");
+		System.out.println(list);
+		
+		
+		
+		
+		
+		MyArrayList list2 = new MyArrayList(100);
+		
+		System.out.println(list2);
+		
+		for (int i=0; i<100; i++) {
+			list2.add(i + "");
+		}
+		
+		System.out.println(list2);
+		
+		
+	}
+
+	private static void m7() {
+		
+		
+		int[] list = new int[4];
+		
+		int[] temp = new int[list.length * 2];
+		
+		//복사
+		
+		list = temp;
+		
+		
+		ArrayList<Integer> nums = new ArrayList<>();
+		
+		System.out.println(nums.size());
+		
+		nums.add(10);
+		nums.add(20);
+		nums.add(30);
+		nums.add(40);
+		
+		nums.add(50);
+		
+		//size(): 내부 배열의 길이(X) > 데이터의 개수(O)
+		System.out.println(nums.size());
+
+//		for (int i=0; i<1000000; i++) {
+//			nums.add(i);
+//		}
+//		
+//		System.out.println(nums.size());
+		
+		//System.out.println(nums);
+		
+	}
 
 	private static void m6() {
 		
@@ -63,78 +240,83 @@ public class Ex54_ArrayList {
 		names.add("참새");
 		
 		//dump
-		//Arrays.toString(배열)
-		//toString() 재정의  
+		//- Arrays.toString(배열)
+		//- toString() 재정의
+		
 		System.out.println(names);
+		
 		
 	}
 
 	private static void m5() {
 		
-		//선택 > 학생 배열(5명) > 데이터 집합?
-		//1.배열 > 길이 고정 > 학생 수 정확(고정) > 배열이 빠름 
-		//2.ArrayList > 길이 가변 > 학생 수 부정확 > ArrayList 사용 
+		//선택? 데이터 집합?
+		//1. 배열      > 길이 고정 > 학생수 정확(고정)
+		//2. ArrayList > 길이 가변 > 학생수 불명확 
 		
-		//성적표 
-		ArrayList<Student> list = new ArrayList<Student>();
+		//성적표
+		ArrayList<Student> list = new ArrayList<Student>(); //학생 명단
 		
 		Random rnd = new Random();
 		
-		String[] names = {"홍길동","아무개","강아지","고양이","병아리"};
-		for(int i=0; i<5; i++) {
+		String[] names = { "홍길동", "아무개", "강아지", "고양이", "병아리" };
+		
+		for (int i=0; i<5; i++) {
+			
 			Student s = new Student(); //학생 1명
 			
-			s.setNo(i+1);
+			s.setNo(i + 1);
 			s.setName(names[i]);
 			
-			//60~100점 사이 
-			//0 ~ 40 + 60 이용
-			s.setKor(rnd.nextInt(41)+60);
-			s.setEng(rnd.nextInt(41)+60);
-			s.setMath(rnd.nextInt(41)+60);
+			//60 ~ 100
+			//0 ~ 40  +  60
+			s.setKor(rnd.nextInt(41) + 60);
+			s.setEng(rnd.nextInt(41) + 60);
+			s.setMath(rnd.nextInt(41) + 60);
 			
-			list.add(s); //학생 명단 추가 
-		
-		
+			list.add(s); //학생 명단에 추가
+			
 		}
-	//출력
+		
+		//출력
 		System.out.println("======================================================");
-		System.out.println("                         성적표");
-
+		System.out.println("                        성적표");
 		System.out.println("======================================================");
 		System.out.println("[번호]\t[이름]\t[국어]\t[영어]\t[수학]\t[총점]\t[평균]");
 		
-		for(Student s : list) {
+		
+		for (Student s : list) {
 			System.out.printf("%5d\t%s\t%5d\t%5d\t%5d\t%5d\t%5.1f\n"
-					,s.getNo()
-					,s.getName()
-					,s.getKor()
-					,s.getEng()
-					,s.getMath()
-					,s.getTotal()
-					,s.getAvg());
+								, s.getNo()
+								, s.getName()
+								, s.getKor()
+								, s.getEng()
+								, s.getMath()
+								, s.getTotal()
+								, s.getAvg());
 		}
 		
-	
+		
 	}
 
 	private static void m4() {
 		
-		//객체 배열 
+		//객체 배열
 		//Cup[] list = new Cup[5];
+		
 		ArrayList<Cup> list = new ArrayList<Cup>();
 		
 		Cup cup = new Cup("white", 5000);
 		list.add(cup);
 		
 		list.add(new Cup("blue", 3000));
-		list.add(new Cup("yellow",4500));
+		list.add(new Cup("yellow", 4500));
 		
-		for(int i=0; i<list.size(); i++) {
+		for (int i=0; i<list.size(); i++) {
 			System.out.println(list.get(i));
 		}
-		System.out.println();
-		for(Cup c:list) {
+		
+		for (Cup c : list) {
 			System.out.println(c);
 		}
 		
@@ -145,30 +327,38 @@ public class Ex54_ArrayList {
 
 	private static void m3() {
 		
-		int[] ns1 = new int[3];
-		int[][] ns2 = new int [2][3];
-		int[][][] ns3 = new int [2][3][4];
+		int[]     ns1 = new int[3];
+		int[][]   ns2 = new int[2][3];
+		int[][][] ns3 = new int[2][3][4];
 		
-//		int ns4[] = new int[3];
+		//int ns4[] = new int[3];
 		
 		//ns1
 		ArrayList<Integer> ms1 = new ArrayList<Integer>();
 		ms1.add(10);
+		
 		//ns2
-		ArrayList<ArrayList<Integer>> ms2 = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> ms2 
+						= new ArrayList<ArrayList<Integer>>();
 		ms2.add(new ArrayList<Integer>());
 		ms2.get(0).add(10);
+		
 		//ns3
-		ArrayList<ArrayList<ArrayList<Integer>>>ms3 = new ArrayList<ArrayList<ArrayList<Integer>>>();
-		ms3.add(new ArrayList<ArrayList<Integer>>());
-		ms3.get(0).get(0).add(10);
+		ArrayList<ArrayList<ArrayList<Integer>>> ms3
+			= new ArrayList<ArrayList<ArrayList<Integer>>>();
+		
+		
 	}
 
 	private static void m2() {
+		
 		//String[] list
 		ArrayList<String> list = new ArrayList<String>();
-		//요소 추가하기 , boolean add(T, value)
-		//배열의 마지막에 추가 -> append mode
+		
+		
+		//1. 요소 추가하기
+		//- boolean add(T value)
+		//- 배열의 마지막에 추가 > Append Mode
 		list.add("바나나");
 		list.add("딸기");
 		list.add("사과");
@@ -176,109 +366,107 @@ public class Ex54_ArrayList {
 		list.add("귤");
 		
 		//2. 요소 개수
-		// int size();
-		
+		//- int size()
 		System.out.println(list.size());
-		
 		
 		//3. 요소 읽기
-		
-		//T get(int index)
-		System.out.println(list.get(0));
-		System.out.println(list.get(1));
+		//- T get(int index)
 		System.out.println(list.get(2));
-		System.out.println(list.get(3));
-		System.out.println(list.get(4)); //IndexOutOfBoundsException
-//		System.out.println("홍길동".charAt(5));
+		//System.out.println(list.get(5)); //IndexOutOfBoundsException
+		//System.out.println("홍길동".charAt(5)); //StringIndexOutOfBoundsException
 		
-		System.out.println(list.get(list.size()-1));
-		System.out.println();
-		//4. 요소 수정 
-		// T set(int index, T newValue)
-		String temp = list.set(2, "파인애플"); 
+		System.out.println(list.get(list.size() - 1));
+		
+		//4. 요소 수정
+		//- T set(int index, T newValue)
+		//list[2] = "파인애플";
+		String temp = list.set(2, "파인애플");
 		System.out.println(temp);
 		System.out.println(list.get(2));
-		System.out.println();
+		
+		
 		//5. 요소 삭제
-		//방 없애기 > 길이 줄어들음
-		//T remove(int index) > 방번호를 찾아서 삭제
-		// boolean remove(T value) > 값을 찾아서 삭제 
-		//시프트 발생 > 삭제된 방 이후로 모든 요소의 방번호 - 1 
-		System.out.println(list.size());
+		//- 방 없애기 > 길이 줄어든다.
+		//- T remove(int index) > 방번호를 찾아서 삭제
+		//- boolean remove(T value) > 값을 찾아서 삭제
+		//- 시프트발생 > 삭제된 방 이후로 모든 요소의 방번호 - 1
+		System.out.println(list.size()); //5
 		System.out.println(list.get(3));
 		list.remove(2);
+		list.remove("파인애플");
+		System.out.println(list.size()); //4
+		System.out.println(list.get(3));
+		
+		
+		//6. 요소 추가
+		//- 요소는 원하는 추가
+		//- Insert Mode
+		//- void add(int index, T value)
+		//- 시프트 발생 > 삽입된 방 이후의 모든 요소는 방번호 + 1
+		System.out.println(list.get(3));
+		list.add(2, "망고");
+		System.out.println(list.get(3));
 		System.out.println(list.size());
-		System.out.println(list.get(3));
-		System.out.println();
-		//6. 요소 추가 
-		// - 요소는 원하는 추가 
-		// Insert Mode 
-		// void add(int index, T value)
-		// 시프트 발생 > 삽입된 방 이후 모든 요소는 방번호 + 1 
-		System.out.println(list.get(3));
-		list.add(2,"망고");
-		System.out.println(list.get(2));
-		System.out.println(list.get(3));
-		System.out.println(list.size());
-		System.out.println();
+		
 		
 		//7. 요소 검색
-		// int indexOf(T value)
-		// int lastIndexOf(T value)
-		//boolean contains(T value)
+		//- int indexOf(T value)
+		//- int lastIndexOf(T value)
+		//- boolean contains(T value)
+		System.out.println(list.indexOf("사과"));
+		System.out.println(list.indexOf("파인애플"));
+		System.out.println(list.indexOf("포도"));
 		
-		System.out.println(list.indexOf("파인애플")); //없음
-		System.out.println(list.indexOf("포도")); 
-		list.remove("포도"); //일반적으로 방 번호로 많이 지움 
+		list.remove(3);
+		list.remove("포도");
 		System.out.println(list.indexOf("포도"));
 		System.out.println();
 		
-		//8 요소 탐색
-		for(int i = 0 ; i<list.size(); i++) {
+				
+		//8. 요소 탐색
+		for (int i=0; i<list.size(); i++) {
 			System.out.println(list.get(i));
 		}
-		
 		System.out.println();
 		
 		//for문
-		//- 향상된 for문 (Enhanced for statement)
-		
-//		for(변수 : 배열) {
-//			
-//		}
-		//무조건 처음부터 끝까지 다 가져옴 
-		//무조건 오름차순만 가능 
-		for(String item : list) {
+		//- 향상된 for문(Enhanced for statement)
+		for (String item : list) {
 			System.out.println(item);
 		}
 		System.out.println();
 		
-		//9 초기화
-		// 모든 요소 삭제 > 모든방.remove();
-		//void clear();
+		
+		//9. 초기화
+		//- 모든 요소 삭제 > 모든방.remove()
+		//- void clear()
 		
 		list.clear();
+		
 		System.out.println(list.size());
-		System.out.println(list);
 		
-		//10. 빈 배열 확인
-		//boolean isEmpty()
+		
+		//10. 빈배열 확인
+		//- boolean isEmpty()
 		System.out.println(list.isEmpty());
-		System.out.println(list.size() ==0);
+		System.out.println(list.size() == 0);
+		
 		list.add("사과");
-		System.out.println(list.isEmpty());
 		
-	
+		System.out.println(list.isEmpty());
+		System.out.println(list.size() == 0);
+		
+		
+		
+		
 	}
 
 	private static void m1() {
 		
-		
 		//배열
-		// 타입 명시(int)
-		// 길이 명시(3)
+		//- 타입 명시(int)
+		//- 길이 명시(3)
 		int[] nums1 = new int[3];
-		
 		
 		//요소 접근 > 첨자(index) 사용
 		nums1[0] = 10; //배열[index] > 인덱서(Indexer)
@@ -289,45 +477,47 @@ public class Ex54_ArrayList {
 		System.out.println(nums1[1]);
 		System.out.println(nums1[2]);
 		
-
 		System.out.println(nums1.length);
 		System.out.println();
 		
 		
 		//컬렉션
-		//타입 명시 x > Object 배열
-		//길이 명시 x > 가변 
-//		ArrayList nums2 = new ArrayList();
-		
+		//- 타입 명시(X) > Object 배열
+		//- 길이 명시(X) > 가변
+		//ArrayList nums2 = new ArrayList();
 		ArrayList<Integer> nums2 = new ArrayList<Integer>();
 		
 		//요소 접근
-		//- boolean add (T value)
-		// Append(차례대로 추가)
-		nums2.add(100); //nums2[0] = 100;
-		nums2.add(200); //nums2[1] = 200;
-		nums2.add(300); //nums2[2] = 300;
+		//- boolean add(T value)
+		//- Append(차례대로 추가)
+		nums2.add(100); //nums2[0] = 100
+		nums2.add(200); //nums2[1] = 200
+		nums2.add(300); //nums2[2] = 300
 		
-		
-		System.out.println(nums2.get(0));
+		System.out.println(nums2.get(0)); //nums2[0]
 		System.out.println(nums2.get(1));
 		System.out.println(nums2.get(2));
 		
-		System.out.println(nums2.size());
+		System.out.println(nums2.size()); //길이 3
 		
 		nums2.add(400);
 		nums2.add(500);
-		nums2.add(600);
-		System.out.println(nums2.size());
 		
+		System.out.println(nums2.size()); //길이 5
+		System.out.println();
 		
-		for( int i=0; i<nums2.size(); i++) {
+		for (int i=0; i<nums2.size(); i++) {
 			System.out.println(nums2.get(i));
 		}
 		
+		
 	}
+
 }
-class Cup{
+
+
+class Cup {
+	
 	private String color;
 	private int price;
 	
@@ -340,9 +530,11 @@ class Cup{
 	public String toString() {
 		return "Cup [color=" + color + ", price=" + price + "]";
 	}
+	
 }
 
-class Parent{
+
+class Parent {
 	
 	private int a;
 	private int b;
@@ -366,10 +558,10 @@ class Parent{
 		System.out.println("부모 인사");
 	}
 	
-	
 }
 
-class Child extends Parent{
+class Child extends Parent {
+	
 	private int c;
 	private int d;
 	
@@ -378,21 +570,16 @@ class Child extends Parent{
 //		this.d = 0;
 //	}
 	
-	//부모의 기본 생성자를 호출함 -> 기본 생성자 주석처리하면 에러가 발생 
-	//부모 생성자를 호출할 때 기본 생성자가 아닌 본인이 원하는 부모 생성자 호출하기 위해선 super 사용 
-	//부모 생성자를 명시적으로 호출 -> super();
-	//부모 생성자의 기본값을 호출 할 땐 super();
-	//특정 생성자를 부를 땐 숫자 대입해야함 
-	//컴파일러 자체에서 자동으로 super()를 넣어서 그럼 
-	public Child(int c, int d) { 
-		super(0,0);
+	public Child(int c, int d) {
+		super(0, 0); //부모 생성자를 명시적으로 호출
+		//super();
 		this.c = c;
 		this.d = d;
 		
-		
 		//객체 접근 연산자
 		//1. this > 자기 자신
-		//2. super > 자기 부모 (상속하는 클래스)
+		//2. super > 자기 부모
+		
 	}
 
 	@Override
@@ -402,10 +589,33 @@ class Child extends Parent{
 	
 	@Override
 	public void hello() {
+		//super.hello();
 		System.out.println("자식 인사");
 	}
 	
 	public void temp() {
 		super.hello();
 	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ javatest1/src/com/test/java/collection/Ex55_Stack.java
@@ -0,0 +1,35 @@
+package com.test.java.collection;
+
+public class Ex55_Stack {
+public static void main(String[] args) {
+	
+	/*
+	 
+	 ADT, Abstract Data Type(추상적 자료형)
+	 - 배열 + 사용법 용도 구현 
+	 - Stack
+	 - Queue
+	 
+	 Stack
+	 	- 후입선출
+	 	- LIFO , Last In First Output
+	 	- 저장소에 나중에 들어간 요소가 가장 먼저 나온다.
+	 ex) 메모리 구조(Stack)
+	 ex) 되돌리기 Ctrl Z , 다시하기 Ctrl Y 
+	 ex) 브라우저 > 뒤로 가기, 앞으로 가기 
+	 
+	 
+	 
+	 Queue
+	 	-선입선출
+	 	-FIFO , First In First Output
+	 	-저장소에 먼저 들어간 요소가 가장 먼저 나온다.
+	 ex) 줄서기
+	 ex) 주문리스트 
+	 ex) 순서가 있는 모든 목록 
+	 
+	 */
+	
+	
+}//main
+}
