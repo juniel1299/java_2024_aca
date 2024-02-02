0abc082f414fd1f38b855ccee489543e8f267516
juniel99 <youngs5440@gmail.com>
2024-02-02 15:21:29 +0900

    ldsghgkhlsdgkhgdskl;ghdslkgsd

--- ViewTest/src/com/test/java/viewfolder/TestView.java
+++ ViewTest/src/com/test/java/viewfolder/TestView.java
@@ -4,8 +4,21 @@ public class TestView {
 
 	public static void main(String[] args) {
 		
-		ViewAll.seasonPassRegisterTop();
+		ViewAll.adminMainView();
+		ViewAll.back();
+		ViewAll.bottomAdd();
+		ViewAll.bottomDelete();
+		ViewAll.bottomReceive();
+		ViewAll.bottomResign();
+		ViewAll.calAdd();
+		ViewAll.calAddResult();
+		ViewAll.calDeleteMid();
+		ViewAll.calDeleteResult();
+		ViewAll.calList();
+		ViewAll.calMain();
 		ViewAll.costTimeTable();
+		ViewAll.employeeDeleteOne();
+		ViewAll.employeeDeleteResult();
 		
 		
 	}//main
