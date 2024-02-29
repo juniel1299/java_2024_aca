5fe18e908b17ff16487af9208821db3cd733caab
juniel1299 <youngs5440@gmail.com>
2024-02-29 20:17:14 +0900

    3

--- JDBCTest/src/com/test/JDBC/script.sql
+++ JDBCTest/src/com/test/JDBC/script.sql
@@ -157,5 +157,5 @@ declare
     show user;
     
     alter session set "_oracle_script" = true;
-    create user jdbc identified by java1234;
-    grant connect, resource, dba to jdbc;
+    create user mainprojecthome identified by java1234;
+    grant connect, resource, dba to mainProjecthome;
